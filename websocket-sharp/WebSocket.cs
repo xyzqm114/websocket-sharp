728eea6c5dd67c33b81d366a65ddc7ad962fbb99
sta <sta.blockhead@gmail.com>
2025-11-28 16:12:27 +0900

    [Modify] Polish it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -872,7 +872,7 @@ namespace WebSocketSharp
     public ClientSslConfiguration SslConfiguration {
       get {
         if (!_isClient) {
-          var msg = "The get operation is not available.";
+          var msg = "The interface is not for the client.";
 
           throw new InvalidOperationException (msg);
         }
