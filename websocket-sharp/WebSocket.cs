05311680af14b870507e0616e60531595d022996
sta <sta.blockhead@gmail.com>
2025-12-30 16:45:54 +0900

    [Modify] Polish it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -4494,7 +4494,7 @@ namespace WebSocketSharp
     {
       lock (_forState) {
         if (!canSet ()) {
-          var msg = "The SetUserHeader method is not available.";
+          var msg = "The current state of the interface is neither New nor Closed.";
 
           throw new InvalidOperationException (msg);
         }
